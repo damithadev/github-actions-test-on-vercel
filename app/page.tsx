391fff06dbde36f49b32c0d1579e345c4d7107fc
Damitha <hellodamitha@gmail.com>
2024-05-20 20:21:56 +0530

    nextjs 14.2.3 installation

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h2 className="mb-3 text-2xl font-semibold">
+        This is a test deployment of a Next.js app on Vercel
+      </h2>
+      <p>damitha.dev</p>
+    </main>
+  );
+}
