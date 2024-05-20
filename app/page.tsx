8a074d3bf4684c234eaa82c438eba882b5f139a9
Damitha <hellodamitha@gmail.com>
2024-05-20 20:23:31 +0530

    minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h2 className="mb-3 text-2xl font-semibold">
-        This is a test deployment of a Next.js app on Vercel
+        This is a test deployment of a Next.js app on Vercel using GitHub
+        Actions.
       </h2>
       <p>damitha.dev</p>
     </main>
